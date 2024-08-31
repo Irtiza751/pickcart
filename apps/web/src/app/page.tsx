84d272c826319cf1d2ea126dc284fe26ba3a0fd3
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-09-01 01:42:15 +0500

    limit the products to 12

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,20 +10,20 @@ export interface Product {
 }
 
 export default async function Home() {
-  const response = await request("/products?limit=10");
+  const response = await request("/products?limit=12");
   const products: Product[] = await response.json();
 
   return (
     <main className="container h-full">
       <section className="grid grid-cols-3 gap-4 h-full mt-4">
-        <Product featured={true} name="Black Puffert Jacket" price={12000} />
+        {/* <Product featured={true} name="Black Puffert Jacket" price={12000} /> */}
         {products.map((product) => (
           <Product
             key={product.id}
             name={product.title}
             price={product.price}
             // image={product.image}
-            inStock={false}
+            // inStock={false}
           />
         ))}
       </section>
