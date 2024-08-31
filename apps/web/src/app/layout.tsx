68766638cf4049a5d2415ecc70b8d22125532681
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2024-09-01 00:54:50 +0500

    Add the footer element

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-proider";
 import { Navbar } from "@/components/navbar";
+import { Pickcart } from "@/components/logo";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,17 +19,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`min-h-screen ${inter.className}`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <>
-            <Navbar />
-            {children}
-          </>
+          <Navbar />
+          {children}
+          <footer className="py-4 text-center border-t mt-4 flex justify-center">
+            <Pickcart width="120" height="30" />
+          </footer>
         </ThemeProvider>
       </body>
     </html>
